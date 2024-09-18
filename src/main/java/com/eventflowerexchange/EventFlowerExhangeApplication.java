f1c51b445d07d9eb284f937313be84ba554c49b5
Osi30 <nthanhn2491@gmail.com>
2024-09-18 14:04:35 +0700

    Upload Spring Boot Backend Project

---
+++ src/main/java/com/eventflowerexchange/EventFlowerExhangeApplication.java
@@ -0,0 +1,13 @@
+package com.eventflowerexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFlowerExhangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFlowerExhangeApplication.class, args);
+	}
+
+}
