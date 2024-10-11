6cc4b318cf2252681cfe2fb0793445ac6363f1c5
DESKTOP-R94SMKK\2111d <khanhtrantuc2004@gmail.com>
2024-10-11 21:14:08 +0700

    Update Bankingconfig

---
+++ src/main/java/com/eventflowerexchange/api/PaymentAPI.java
@@ -0,0 +1,9 @@
+package com.eventflowerexchange.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PaymentAPI {
+}
